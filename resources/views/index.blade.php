b8e9366a2f705cc524976a77f4b37abcdc404e42
devjubis <castro.ju837@gmail.com>
2025-01-17 22:00:48 -0300

    :

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -190,9 +190,9 @@
   <div class="container-fluid">
     <div class="row">
       @include('components.navegacao')
-      <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+      <m!ain class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
         @yield('content')     
-      </main>
+      </m!ain>
     </div>
   </div>
 =======
